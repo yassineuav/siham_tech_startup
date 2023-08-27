b92eb1b9560c36ccc1586f757494ed70e8f4781a
Yassine Khaidouch <91646673+yassineuav@users.noreply.github.com>
2023-08-27 13:35:21 +0000

    about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,7 +1,28 @@
 'use client';
 
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { fadeInIn, staggerContainer, textVariant } from '../utils/motion';
+
+import { TypingText } from '../components';
+
 const About = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className='gradient-02 z-0'>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{once: false, amount:0.25}}
+        className={`${styles.innerWidth} max-auto ${styles.flexCenter} flex-col`}
+      >
+        <TypingText 
+          title="| About Drone Delivery" 
+          textStyle="text-center"/>
+      </motion.div>
+    </div>
     About section
   </section>
 );
